a4ba64a9e959c85f1eb72024c33c15f4f693a1bd
Zeeshan Shafiq <zeeshanshafiq001@gmail.com>
2020-07-01 17:07:06 +0500

    update railtie to support sinatra.

--- lib/apollo_upload_server/railtie.rb
+++ lib/apollo_upload_server/railtie.rb
@@ -1,9 +1,14 @@
 require 'apollo_upload_server/middleware'
 
 module ApolloUploadServer
-  class Railtie < Rails::Railtie
-    initializer 'apollo_upload_server.apply_middleware' do |app|
-      app.middleware.use Middleware
+  def self.included(base)
+      base.class_eval do
+        use MiddleWare
+      end
     end
-  end
+#   class Railtie < Rails::Railtie
+#     initializer 'apollo_upload_server.apply_middleware' do |app|
+#       app.middleware.use Middleware
+#     end
+#   end
 end
